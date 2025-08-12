6b87cac65e9390131e53be8718492e0635ef65cf
Mohsin <in.mohsin@outlook.com>
2025-08-12 23:43:15 +0530

    Remove breadcrumbs from tags or folders

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -46,7 +46,7 @@ export const defaultContentPageLayout: PageLayout = {
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [
-    Component.Breadcrumbs(), 
+    // Component.Breadcrumbs(), 
     Component.ArticleTitle(), 
     Component.ContentMeta()
   ],
