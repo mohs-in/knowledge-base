15ce7d0546c8b7587676e7e5780742fac4519be8
Mohsin <in.mohsin@outlook.com>
2025-12-06 12:57:09 +0530

    fix Quartz ogImage TS error caused by React 19 typings

--- quartz/plugins/emitters/ogImage.tsx
+++ quartz/plugins/emitters/ogImage.tsx
@@ -49,7 +49,7 @@ async function generateSocialImage(
     iconBase64,
   })
 
-  const svg = await satori(imageComponent, {
+  const svg = await satori(imageComponent as any, {
     width,
     height,
     fonts,
